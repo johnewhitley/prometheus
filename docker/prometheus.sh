4615add76125c89be360374590cd253d448cfb19
John Edward Whitley IV <john.whitley@mckesson.com>
2018-03-02 02:35:54 -0500

    Added some more stuff

--- docker/prometheus.sh
+++ docker/prometheus.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo docker build -t my-prometheus .
-sudo docker run --name="prometheus" -d -p 9090:9090 my-prometheus
-sudo docker exec -it prometheus bash
+sudo docker run --name prometheus -d -p 9090:9090 my-prometheus
