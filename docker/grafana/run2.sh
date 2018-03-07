634917cc75e17d0e7369ada798307be3f7857e47
johnewhitley <johnewhitley@gmail.com>
2018-03-07 18:04:34 -0500

    Fixing datasource

--- docker/grafana/run2.sh
+++ docker/grafana/run2.sh
@@ -1,11 +1,10 @@
 echo 'Starting Grafana...'
-/run.sh "$@" &
 AddDataSource() {
-  curl 'http://localhost:3000/api/datasources' \
+  curl 'http://whitleyserver.ddns.net:3000/api/datasources' \
     -X POST \
     -H 'Content-Type: application/json;charset=UTF-8' \
     --data-binary \
-    '{"name":"Prometheus","type":"prometheus","url":"http://prometheus:9090","access":"proxy","isDefault":true}'
+    '{"name":"Prometheus","type":"prometheus","url":"http://whitleyserver.ddns.net:9090","access":"proxy","isDefault":true}'
 }
 until AddDataSource; do
   echo 'Configuring Grafana...'
