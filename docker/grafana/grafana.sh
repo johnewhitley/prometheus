e7474b08369d5535910430822004513c8c5a0071
johnewhitley <johnewhitley@gmail.com>
2018-03-08 10:27:24 -0500

    Automation

--- docker/grafana/grafana.sh
+++ docker/grafana/grafana.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker build -t my-grafana ~/prometheus/docker/grafana
-sudo docker run --name grafana -d -p 3000:3000 grafana/grafana
+sudo docker run --name grafana -d -p 3000:3000 my-grafana
