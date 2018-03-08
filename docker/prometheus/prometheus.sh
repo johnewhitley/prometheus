7440c1c9b538da55552660071fde0e689a0f8e0e
John Edward Whitley IV <john.whitley@mckesson.com>
2018-03-08 16:32:32 -0500

    Added logic to start.sh and fixed a few vars

--- docker/prometheus/prometheus.sh
+++ docker/prometheus/prometheus.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo docker build -t my-prometheus ~/prometheus/docker/prometheus
 sudo docker run --name prometheus -d -p 9090:9090 my-prometheus
-/bin/bash ~/prometheus/docker/grafana/grafana.sh
--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-/bin/bash ~/prometheus/docker/prometheus/prometheus.sh
+
+checkprometheus() {
+  if [ -n "$(sudo docker ps -q -f name=prometheus)" ]; then
+    echo "Prometheus Container is already running.\n"
+  else
+    echo "Running clean up on Prometheus.\n"
+    sudo docker rm prometheus
+    # Call to build and run containers
+    echo "Building and starting Prometheus Container\n"
+    /bin/bash ~/prometheus/docker/prometheus/prometheus.sh
+  fi
+}
+
+checkgrafana() {
+  #!/bin/bash
+  if [ -n "$(sudo docker ps -q -f name=grafana)" ]; then
+    echo "Grafana Container is already running.\n"
+  else
+    echo "Running clean up on Grafana.\n"
+    sudo docker rm grafana
+    # Call to build and run containers
+    echo "Building and starting Grafana Container.\n"
+    /bin/bash ~/prometheus/docker/grafana/grafana.sh
+    echo "Grafana Container is now up.\n"
+}
+
+#Main
+checkprometheus
+checkgrafana
