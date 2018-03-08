22450294f491638f5f1dde68acd36b2fdfc45c87
John Edward Whitley IV <john.whitley@mckesson.com>
2018-03-08 16:38:24 -0500

    Added logic to start.sh and fixed a few vars

--- docker/start.sh
+++ docker/start.sh
@@ -6,20 +6,19 @@ checkprometheus() {
   else
     echo "Running clean up on Prometheus.\n"
     sudo docker rm prometheus
-    # Call to build and run containers
+    # Call to build and run container
     echo "Building and starting Prometheus Container\n"
     /bin/bash ~/prometheus/docker/prometheus/prometheus.sh
   fi
 }
 
 checkgrafana() {
-  #!/bin/bash
   if [ -n "$(sudo docker ps -q -f name=grafana)" ]; then
     echo "Grafana Container is already running.\n"
   else
     echo "Running clean up on Grafana.\n"
     sudo docker rm grafana
-    # Call to build and run containers
+    # Call to build and run container
     echo "Building and starting Grafana Container.\n"
     /bin/bash ~/prometheus/docker/grafana/grafana.sh
     echo "Grafana Container is now up.\n"
